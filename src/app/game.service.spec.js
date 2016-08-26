8c8758edc031df47bf65ff63dbaf3d6d4bca5fa9
yaroslav polubiedov <y.polubiedov@studytube.nl>
2016-08-26 09:54:21 +0300

    commit after building

---
+++ src/app/game.service.spec.js
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var testing_1 = require('@angular/core/testing');
+var game_service_1 = require('./game.service');
+describe('Service: Game', function () {
+    beforeEach(function () {
+        testing_1.addProviders([game_service_1.GameService]);
+    });
+    it('should ...', testing_1.inject([game_service_1.GameService], function (service) {
+        expect(service).toBeTruthy();
+    }));
+});
+//# sourceMappingURL=game.service.spec.js.map
---
+++ src/app/health/health.component.spec.js
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var health_component_1 = require('./health.component');
+describe('Component: Health', function () {
+    it('should create an instance', function () {
+        var component = new health_component_1.HealthComponent();
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=health.component.spec.js.map
